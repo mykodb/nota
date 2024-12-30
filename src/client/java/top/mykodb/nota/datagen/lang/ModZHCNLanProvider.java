846e94e810d0d25caafd25044d0d625cca5052cb
mykod <mykod@outlook.com>
2024-12-30 14:46:14 +0800

    Initial commit

---
+++ src/client/java/top/mykodb/nota/datagen/lang/ModZHCNLanProvider.java
@@ -0,0 +1,21 @@
+package top.mykodb.nota.datagen.lang;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
+import net.minecraft.registry.RegistryWrapper;
+import top.mykodb.nota.register.ModItem;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModZHCNLanProvider extends FabricLanguageProvider {
+    public ModZHCNLanProvider(FabricDataOutput dataOutput, CompletableFuture<RegistryWrapper.WrapperLookup> registryLookup) {
+        super(dataOutput, "zh_cn", registryLookup);
+    }
+
+    @Override
+    public void generateTranslations(RegistryWrapper.WrapperLookup registryLookup, TranslationBuilder ling) {
+        //ling.add()
+        ling.add(ModItem.Companion.getLING_SHI(),"灵石" );
+
+    }
+}
